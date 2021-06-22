c1609b883155d37d1eb3bbdcded78cde7fe072bd
Debora Durek <debora.durek@gmail.com>
2021-06-22 16:11:42 -0500

    feat: Add buttons to steppers component

--- utils/components/UdaciSteppers.js
+++ utils/components/UdaciSteppers.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 
-class UdaciSteppers extends Component {
-  render() {
-    return (
+function UdaciSteppers({ unit, value, onIncrement, onDecrement }) {
+  return (
+    <View>
       <View>
-        <Text>UdaciSteppers</Text>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name="minus" size={30} color={'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name="plus" size={30} color={'black'} />
+        </TouchableOpacity>
       </View>
-    );
-  }
+      <View>
+        <Text>{value}</Text>
+        <Text> {unit} </Text>
+      </View>
+    </View>
+  );
 }
 
 export default UdaciSteppers;
