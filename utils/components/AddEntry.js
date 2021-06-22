62e68e266d4e243c9a342f4e40c854736e6d7f6e
Debora Durek <debora.durek@gmail.com>
2021-06-21 20:03:35 -0500

    feat: Add methods to increment and decrement entry

--- utils/components/AddEntry.js
+++ utils/components/AddEntry.js
@@ -3,6 +3,50 @@ import { View } from 'react-native';
 import { getMetricMetaInfo } from '../helpers';
 
 class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  };
+
+  /* Methods for run, bike and swim metrics only */
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] + step;
+
+      return {
+        ...currentState,
+        [metric]: count > max ? max : count,
+      };
+    });
+  };
+
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] - step;
+
+      return {
+        ...currentState,
+        [metric]: count < 0 ? 0 : count,
+      };
+    });
+  };
+
+  /* Method for sleep and eat only */
+
+  slider = (metric, value) => {
+    this.setState({
+      [metric]: value,
+    });
+  };
+
   render() {
     return <View>{getMetricMetaInfo('bike').getIcon()}</View>;
   }
