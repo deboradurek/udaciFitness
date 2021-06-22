d509f55644476a9aa6e7e38038a15b4af21ef315
Debora Durek <debora.durek@gmail.com>
2021-06-22 15:55:11 -0500

    feat: Add slider component

--- utils/components/UdaciSlider.js
+++ utils/components/UdaciSlider.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
+import Slider from '@react-native-community/slider';
 
-class UdaciSlider extends Component {
-  render() {
-    return (
+function UdaciSlider({ max, unit, step, value, onChange }) {
+  return (
+    <View>
+      <Slider
+        value={value}
+        step={step}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
       <View>
-        <Text>UdaciSlider</Text>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default UdaciSlider;
