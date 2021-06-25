377516b8b500dcef8adc0d47eeb2e4b1ffa250c9
Debora Durek <debora.durek@gmail.com>
2021-06-25 17:49:26 -0500

    feat: Add customised status bar using StatusBar

--- App.js
+++ App.js
@@ -4,17 +4,25 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
 import AddEntry from './components/AddEntry';
-import { Text, View, Platform } from 'react-native';
+import { Platform, StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import 'react-native-gesture-handler';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { purple, white } from './utils/colors';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
 
 const Tab = Platform.OS === 'ios' ? createBottomTabNavigator() : createMaterialTopTabNavigator();
 
-// Main
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 
 class App extends Component {
   // Uncomment to reset local data:
@@ -24,6 +32,9 @@ class App extends Component {
     return (
       <Provider store={createStore(reducer)}>
         <NavigationContainer>
+          {/* <SafeAreaView> */}
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          {/* </SafeAreaView> */}
           <Tab.Navigator
             screenOptions={({ route }) => ({
               tabBarIcon: ({ color, size }) => {
