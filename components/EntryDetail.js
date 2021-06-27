aff0debe3371f9c6523835df1847b921bde2636d
Debora Durek <debora.durek@gmail.com>
2021-06-26 21:51:38 -0500

    feat: Get redux state and apply styles

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import MetricCard from './MetricCard';
+import { white } from '../utils/colors';
 
 class EntryDetail extends Component {
   render() {
-    const { date } = this.props.route.params;
+    const { date, ...metrics } = this.props.metrics[0];
 
     return (
-      <View>
-        <Text>Entry Detail - {date}</Text>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
       </View>
     );
   }
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+});
+
+function mapStateToProps(state, { route }) {
+  const { date } = route.params;
+
+  return {
+    metrics: state[date],
+  };
+}
+
+export default connect(mapStateToProps)(EntryDetail);
