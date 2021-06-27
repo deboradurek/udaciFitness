f5caa7e020203381e86f02ea398bbf79fe25b748
Debora Durek <debora.durek@gmail.com>
2021-06-26 19:48:01 -0500

    feat: Create stack navigator

--- App.js
+++ App.js
@@ -4,18 +4,19 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import History from './components/History';
 import AddEntry from './components/AddEntry';
+import EntryDetail from './components/EntryDetail';
+import 'react-native-gesture-handler';
 import { Platform, StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import { purple, white } from './utils/colors';
-// import { SafeAreaView } from 'react-native-safe-area-context';
 import Constants from 'expo-constants';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
-const Tab = Platform.OS === 'ios' ? createBottomTabNavigator() : createMaterialTopTabNavigator();
-
+// Status Bar
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
     <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
@@ -24,47 +25,76 @@ function UdaciStatusBar({ backgroundColor, ...props }) {
   );
 }
 
+// Create TabNavigator
+const Tab = Platform.OS === 'ios' ? createBottomTabNavigator() : createMaterialTopTabNavigator();
+
+function TabNav() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let icon;
+          if (route.name === 'History') {
+            icon = <Ionicons name="ios-bookmarks" size={size} color={color} />;
+          } else if (route.name === 'AddEntry') {
+            icon = <FontAwesome name="plus-square" size={size} color={color} />;
+          }
+          return icon;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        inactiveTintColor: 'gray',
+        style: {
+          backgroundColor: Platform.OS === 'ios' ? white : purple,
+          shadowColor: 'rgba(0,0,0,0.24)',
+          shadowOffset: {
+            width: 0,
+            height: 3,
+          },
+          shadowRadius: 6,
+          shadowOpacity: 1,
+        },
+      }}
+    >
+      <Tab.Screen name="History" component={History} />
+      <Tab.Screen name="AddEntry" component={AddEntry} />
+    </Tab.Navigator>
+  );
+}
+
+// Create StackNavigator
+const Stack = createStackNavigator();
+
+function StackNav() {
+  return (
+    <Stack.Navigator initialRouteName="History" headerMode="screen">
+      <Stack.Screen name="History" component={TabNav} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="EntryDetail"
+        component={EntryDetail}
+        options={{
+          headerBackTitleVisible: false,
+          headerTintColor: white,
+          headerStyle: { backgroundColor: purple },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
 class App extends Component {
   // Uncomment to reset local data:
-  // AsyncStorage.clear()
+  // componentDidMount() {
+  //   AsyncStorage.clear();
+  // }
 
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <NavigationContainer>
-          {/* <SafeAreaView> */}
           <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
-          {/* </SafeAreaView> */}
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color, size }) => {
-                let icon;
-                if (route.name === 'History') {
-                  icon = <Ionicons name="ios-bookmarks" size={size} color={color} />;
-                } else if (route.name === 'AddEntry') {
-                  icon = <FontAwesome name="plus-square" size={size} color={color} />;
-                }
-                return icon;
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: Platform.OS === 'ios' ? purple : white,
-              inactiveTintColor: 'gray',
-              style: {
-                backgroundColor: Platform.OS === 'ios' ? white : purple,
-                shadowColor: 'rgba(0,0,0,0.24)',
-                shadowOffset: {
-                  width: 0,
-                  height: 3,
-                },
-                shadowRadius: 6,
-                shadowOpacity: 1,
-              },
-            }}
-          >
-            <Tab.Screen name="History" component={History} />
-            <Tab.Screen name="AddEntry" component={AddEntry} />
-          </Tab.Navigator>
+          <StackNav />
         </NavigationContainer>
       </Provider>
     );
