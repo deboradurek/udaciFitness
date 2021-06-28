b0ad87be67714f07869dfc210181a11a89bff622
Debora Durek <debora.durek@gmail.com>
2021-06-28 13:34:19 -0500

    feat: Add UI to status is denied or undetermined

--- components/Live.js
+++ components/Live.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 class Live extends Component {
   state = {
     coord: null,
-    status: null,
+    status: 'denied',
     direction: '',
   };
+
+  askPermission = () => {};
 
   render() {
     const { coord, status, direction } = this.state;
 
-    // If the user hasn't given access permission yet
+    // If the user hasn't yet given permission to access their location
     if (status === null) {
       return <ActivityIndicator style={{ marginTop: 30 }} />;
     }
 
-    // If the user denied access permission
+    // If the user denied permission to access their location
     if (status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling
+            location services for this app.
+          </Text>
         </View>
       );
     }
 
+    // If the user neither denies nor grants permission to access their location
     if (status === 'undetermined') {
       return (
-        <View>
-          <Text>Undetermined</Text>
+        <View style={styles.center}>
+          <Foundation name="alert" size={50} />
+          <Text>You need to enable location services for this app.</Text>
+          <TouchableOpacity onPress={this.askPermission} style={styles.button}>
+            <Text style={styles.buttonText}>Enable</Text>
+          </TouchableOpacity>
         </View>
       );
     }
 
+    // If the user grants permission to access their location
     return (
       <View>
         <Text>{JSON.stringify(this.state)}</Text>
@@ -40,5 +54,32 @@ class Live extends Component {
     );
   }
 }
+
+/* Styles */
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
 
 export default Live;
